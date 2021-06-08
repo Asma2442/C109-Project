a9d1380e027ce20440aa1e6d26e52b931b92e2a5
Asma2442 <70644511+Asma2442@users.noreply.github.com>
2021-06-09 04:15:34 +0530

    Update studentPerformance.py

--- studentPerformance.py
+++ studentPerformance.py
@@ -1,7 +1,6 @@
 import plotly.figure_factory as ff
 import pandas as pd
 import csv
-import plotly.graph_objects as go
 import statistics
 import random
 #reading scores data
@@ -16,13 +15,8 @@ mode = statistics.mode(data)
 first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
 second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
 third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
-#Plotting the chart, and lines for mean, 1 standard deviation and 2 standard deviations
+#Plotting the chart
 fig = ff.create_distplot([data], ["reading scores"], show_hist=False)
-fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
-fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1"))
-fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1"))
-fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
-fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
 fig.show()
 #Printing the findings
 list_of_data_within_1_std_deviation = [result for result in data if result > first_std_deviation_start and result < first_std_deviation_end]
